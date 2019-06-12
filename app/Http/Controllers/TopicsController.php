84e8f3b1053440320966f9dfe9d697a9e074b8d5
wangkai <1907341853@qq.com>
2019-06-12 17:07:54 +0800

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -24,8 +24,13 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -42,7 +47,7 @@ class TopicsController extends Controller
         $topic->save();
 
 //		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+		return redirect()->to($topic->link())->with('success', '帖子创建成功！');
 	}
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
@@ -80,7 +85,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('message', 'Updated successfully.');
 	}
 
 	public function destroy(Topic $topic)
