052ea20319706471547dd0657bf37a10872a5262
wangkai <1907341853@qq.com>
2019-06-16 19:55:16 +0800

    删除回话，删除回复

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -57,5 +57,10 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
-
+    //更新回复数
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
