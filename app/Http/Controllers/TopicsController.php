42c975bcb4c813ab3a0cee204cd369496f91da3a
wangkai <1907341853@qq.com>
2019-06-26 16:49:46 +0800

    侧边栏资源推荐

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -19,11 +20,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic, User $user)
+	public function index(Request $request, Topic $topic, User $user, Link $link)
 	{
         $topics = $topic->withOrder($request->order)->paginate(20);
         $active_users = $user->getActiveUsers();
-		return view('topics.index', compact('topics','active_users'));
+        //边栏资源推荐
+        $links = $link->getAllCached();
+		return view('topics.index', compact('topics','active_users','links'));
 	}
 
     public function show(Request $request, Topic $topic)
